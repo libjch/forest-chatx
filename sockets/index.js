21cecbbd19a3d7c43212fb088c6b689b325f121c
JC Libbrecht <jc.libbrecht@takeeateasy.be>
2016-11-15 22:17:11 +0100

    Authenticate send messages as well

--- sockets/index.js
+++ sockets/index.js
@@ -1,6 +1,21 @@
 'use strict';
 
+
+
 module.exports = function(server,services){
+
+    function authenticatedRequest(request,callback){
+        var user = services.users.getOrCreateUser(request.username,request.password);
+        if(user.error){
+            callback({
+                status: 'KO',
+                error: user.error
+            })
+            return false;
+        }
+        return true;
+    }
+
     var io = require("socket.io")(server);
 
     var clients = {};
@@ -8,9 +23,13 @@ module.exports = function(server,services){
     io.on('connection', function(socket){
         console.log('connection event');
 
-        socket.on('message', function (message,callback) {
-            console.log('message event '+JSON.stringify(message));
-            socket.broadcast.to(message.room).emit('message', message);
+        socket.on('message', function (data,callback) {
+            console.log('message event '+JSON.stringify(data));
+            if(!authenticatedRequest(data,callback)) {
+                return;
+            }
+
+            socket.broadcast.to(data.room).emit('message', data);
             callback({
                 status: 'OK'
             });
@@ -45,30 +64,25 @@ module.exports = function(server,services){
                 return;
             }
 
-            var user = services.users.getOrCreateUser(data.username,data.password);
-            if(user.error){
-                callback({
-                    status: 'KO',
-                    error: user.error
-                })
-            }else{
-                //User is registered
-
-                var roomObject = services.rooms.addUserToRoom(data.username,data.room);
-
-                //add to client list to handle broadcast
-                clients[socket.id] = data.username;
-
-                socket.join(roomObject.name);
-
-                socket.broadcast.to(roomObject.name).emit('joined', data.username);
-
-                callback({
-                    status: 'OK',
-                    users: roomObject.users,
-                    messages: []
-                });
+            if(!authenticatedRequest(data,callback)) {
+                return;
             }
+            //User is registered
+
+            var roomObject = services.rooms.addUserToRoom(data.username,data.room);
+
+            //add to client list to handle broadcast
+            clients[socket.id] = data.username;
+
+            socket.join(roomObject.name);
+
+            socket.broadcast.to(roomObject.name).emit('joined', data.username);
+
+            callback({
+                status: 'OK',
+                users: roomObject.users,
+                messages: []
+            });
         });
     });
     return io;
